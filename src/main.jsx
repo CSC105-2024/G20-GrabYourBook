733b55780bf053b236d5f6c2f63248d6b5ae6b59
Sudakarn <sukarn7149@gmail.com>
2025-04-22 16:45:04 +0700

    feat: push ava book

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c12156b (feat: push ava book)
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import Login from './pages/Login.jsx'
+import Mybook from './pages/Mybook.jsx'
+import Comedy from './pages/Comedy.jsx'
+import Register from './pages/Register.jsx'
+import Drama from './pages/Drama.jsx'
+import Fantasy from './pages/Fantasy.jsx'
+import Horror from './pages/Horror.jsx'
+import Romance from './pages/Romance.jsx'
+import Lgbtq from './pages/Lgbtq.jsx'
+<<<<<<< HEAD
+import DetailAva from './pages/DetailBookava.jsx'
+=======
 import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -14,6 +36,9 @@ import Romance from "./pages/Romance.jsx";
 import Lgbtq from "./pages/Lgbtq.jsx";
 import LoginContext from "./context/LoginContext";
 import React from "react";
+>>>>>>> c64410c (commit home category  final)
+=======
+>>>>>>> c12156b (feat: push ava book)
 
 const router = createBrowserRouter([
   {
@@ -60,11 +85,70 @@ const router = createBrowserRouter([
     
 ]);
 
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/mybook",
+    element: <Mybook/>,
+  },
+  {
+    path: "/comedy",
+    element: <Comedy/>,
+  },
+  
+  {
+    path: "/drama",
+    element: <Drama/>,
+  },
+  {
+    path: "/fantasy",
+    element: <Fantasy/>
+  },
+  {
+    path: "/horror",
+    element: <Horror/>,
+  },
+  {
+    path: "/romance",
+    element: <Romance/>,
+  },
+  {
+    path: "/lgbtq",
+    element: <Lgbtq/>,
+  },
+ {
+    path: "/detailava",
+    element:<DetailAva/>,
+  },
+])
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+  {
+    path: "/detailava",
+    element:<DetailAva/>,
+  },
+
+ 
+])
+=======
 function RootWithContext() {
   const [isLogin, setIsLogin] = useState(() => {
     const saved = localStorage.getItem("isLogin");
     return saved === "true";
   });
+>>>>>>> c64410c (commit home category  final)
+=======
+>>>>>>> c12156b (feat: push ava book)
 
   useEffect(() => {
     localStorage.setItem("isLogin", isLogin);
@@ -77,75 +161,8 @@ function RootWithContext() {
   );
 }
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RootWithContext />
   </StrictMode>
 );
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import HomePage from './pages/HomePage.jsx'
-// import Login from './pages/Login.jsx'
-// import Mybook from './pages/Mybook.jsx'
-// import Comedy from './pages/Comedy.jsx'
-// import Register from './pages/Register.jsx'
-// import Drama from './pages/Drama.jsx'
-// import Fantasy from './pages/Fantasy.jsx'
-// import Horror from './pages/Horror.jsx'
-// import Romance from './pages/Romance.jsx'
-// import Lgbtq from './pages/Lgbtq.jsx'
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage/>,
-//   },
-//   {
-//     path: "/login",
-//     element: <Login/>,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register/>,
-//   },
-//   {
-//     path: "/mybook",
-//     element: <Mybook/>,
-//   },
-//   {
-//     path: "/comedy",
-//     element: <Comedy/>,
-//   },
-
-//   {
-//     path: "/drama",
-//     element: <Drama/>,
-//   },
-//   {
-//     path: "/fantasy",
-//     element: <Fantasy/>
-//   },
-//   {
-//     path: "/horror",
-//     element: <Horror/>,
-//   },
-//   {
-//     path: "/romance",
-//     element: <Romance/>,
-//   },
-//   {
-//     path: "/lgbtq",
-//     element: <Lgbtq/>,
-//   },
-
-// ])
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <RouterProvider router={router}/>
-//   </StrictMode>,
-// )
