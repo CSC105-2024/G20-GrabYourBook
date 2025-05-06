3f8988fc3d12c6077c44476f4ad19174f1edc184
Sudakarn <sukarn7149@gmail.com>
2025-05-06 18:35:51 +0700

    migrate database from prisma schema to data.db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,25 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "./generated/prisma/index.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+export const db = new PrismaClient();
 
+db.$connect().catch((e) => {
+  throw new Error(`Database Connection Error ${e}`);
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
